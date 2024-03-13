60ebd94679d3abf8d1054988477d8777500c8bba
Jonathan Lanson <jlanson@versa-networks.com>
2024-03-13 00:55:53 -0400

    Changed way to load map images

--- src/screens/FloorPlanScreen.js
+++ src/screens/FloorPlanScreen.js
@@ -1,9 +1,9 @@
 import { useState, useEffect } from "react";
 import fillerImg from "../../public/images/filler.png";
 // import ImageZoom from "react-native-image-pan-zoom";
-import { ImageZoom } from "@likashefqet/react-native-image-zoom";
-import { Dimensions } from "react-native";
-import { Image } from "native-base";
+import { Image, Dimensions, View, Text } from 'react-native';
+import ImageZoom from 'react-native-image-pan-zoom';
+import { API_BASE_URL } from "@env";
 
 const FloorPlanImage = (props) => {
   const { item } = props;
@@ -59,47 +59,65 @@ const OutsideImage = () => (
 const FloorPlanScreen = ({ route, navigation }) => {
   const { item } = route.params;
   const [uri, setUri] = useState("");
+  const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+  const screenWidth = Dimensions.get('window').width;
+  const screenHeight = Dimensions.get('window').height;
 
   useEffect(() => {
-    const getImage = async () => {
+    const fetchImageDimensions = async () => {
       try {
-        const url = `https://koi-wise-kiwi.ngrok-free.app/locations/image/${item.mapNode}`;
-        const response = await fetch(url);
-        const blob = await response.blob();
-        const imgUrl = URL.createObjectURL(blob);
-        setUri(imgUrl);
-      } catch (err) {
-        console.log("Something went wrong", err);
+        const [width, height] = await new Promise((resolve, reject) => {
+          Image.getSize(
+            uri,
+            (originalWidth, originalHeight) => resolve([originalWidth, originalHeight]),
+            reject
+          );
+        });
+        const scaleFactor = screenWidth / width;
+        const imageHeight = height * scaleFactor;
+        setDimensions({ width: screenWidth, height: imageHeight });
+      } catch (error) {
+        console.error('Error getting image dimensions:', error);
       }
     };
 
+    if (uri) {
+      fetchImageDimensions();
+    }
+  }, [uri, screenWidth]);
+
+  useEffect(() => {
     if (item.mapNode === "outside") {
       setUri(fillerImg);
     } else {
-      getImage();
+      setUri(`${API_BASE_URL}/locations/image/${item.mapNode}`);
     }
-  }, []);
+  }, [item.mapNode]);
 
-  // const mapImg =
-  //   item?.mapNode === "outside" ? (
-  //     <OutsideImage />
-  //   ) : item.mapNode ? (
-  //     <FloorPlanImage item={item} />
-  //   ) : null;
-
-  // return (
-  //   <ImageZoom
-  //     cropWidth={Dimensions.get("window").width}
-  //     cropHeight={Dimensions.get("window").height}
-  //     imageWidth={200}
-  //     imageHeight={200}
-  //   >
-  //     {mapImg}
-  //   </ImageZoom>
-  // );
-  if (uri) {
-    return <ImageZoom uri={uri} maxScale={10} />;
-  }
+  return (
+    <View style={{ flex: 1 }}>
+      {uri && dimensions.height ? (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <ImageZoom
+            cropWidth={screenWidth * 0.9}
+            cropHeight={screenHeight}
+            imageWidth={dimensions.width}
+            imageHeight={dimensions.height}
+            enableCenterFocus={false}
+          >
+            <Image
+              style={{ width: dimensions.width, height: dimensions.height, resizeMode: 'contain' }}
+              source={{ uri }}
+            />
+          </ImageZoom>
+        </View>
+      ) : (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Text>Loading...</Text>
+        </View>
+      )}
+    </View>
+  );
 };
 
 export default FloorPlanScreen;
